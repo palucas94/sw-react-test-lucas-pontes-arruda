b5716372a32fab734c1102f1707cfa0fc9669df7
Lucas Pontes Arruda <paluke94@gmail.com>
2022-05-18 06:34:57 -0300

    [init] initial commit :tada:

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default App;
