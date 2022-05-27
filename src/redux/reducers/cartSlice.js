fcf59a07b1489c69d9c1c279c62e46265b83fc06
Lucas Pontes Arruda <paluke94@gmail.com>
2022-05-27 07:42:35 -0300

    [feat] add cartSlice in redux

---
+++ src/redux/reducers/cartSlice.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    currentProduct: {
+      id: '',
+      attributes: [],
+      qty: 1,
+    },
+    cart: [],
+    cartQty: 0,
+  },
+  reducers: {
+    setInitialProductAttributes: (state, action) => {
+      const { id: currentId, attributes } = state.currentProduct;
+      const { id, product } = action.payload;
+      const { attributes: productAttr } = product;
+
+      if (currentId !== id) {
+        attributes.length = 0;
+        state.currentProduct.id = id;
+      }
+
+      if (productAttr.length) {
+        productAttr.forEach(({ name, type, items }) => (
+          attributes.push({ name, type, value: items[0].value })));
+      }
+    },
+
+    setCurrentProductAttributes: (state, action) => {
+      const { attributes } = state.currentProduct;
+      const { name, type, value } = action.payload;
+
+      const index = attributes.findIndex((i) => i.name === name);
+      attributes[index] = { name, type, value };
+    },
+
+    addToCart: (state) => {
+      const { cart, currentProduct } = state;
+      const { id, attributes } = currentProduct;
+
+      const inCart = cart.find((p) => (
+        p.id === id && JSON.stringify(p.attributes) === JSON.stringify(attributes)
+      ));
+
+      if (inCart) {
+        const index = cart.findIndex((p) => p === inCart);
+        cart[index].qty += 1;
+      } else {
+        cart.push(currentProduct);
+      }
+
+      state.cartQty += 1;
+    },
+  },
+});
+
+export const {
+  fetchAllproducts, addToCart, setInitialProductAttributes, setCurrentProductAttributes,
+} = cartSlice.actions;
+export default cartSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoryReducer from './reducers/categorySlice';
 import currencyReducer from './reducers/currencySlice';
+import cartReducer from './reducers/cartSlice';
 
 const store = configureStore({
   reducer: {
     category: categoryReducer,
     currency: currencyReducer,
+    cart: cartReducer,
   },
 });
 
