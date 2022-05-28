a1e3770bbede94d91d30bf4e6c0418c84afd282b
Lucas Pontes Arruda <paluke94@gmail.com>
2022-05-28 05:18:49 -0300

    [feat] add CartProductCard component

---
+++ src/components/CartProductCard/CartProductCard.jsx
@@ -0,0 +1,122 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import store from '../../redux/store';
+import client from '../../services/apolloClient/client';
+import GET_PRODUCT_BY_ID from '../../services/graphqlQueries/getProductByIdQuery';
+import './CartProductCard.css';
+
+class CartProductCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: '',
+      currency: '',
+    };
+  }
+
+  componentDidMount() {
+    this.fetchProduct();
+    this.setState({
+      currency: store.getState().currency.currentCurrency,
+    });
+
+    store.subscribe(() => {
+      this.setState({
+        currency: store.getState().currency.currentCurrency,
+      });
+    });
+  }
+
+  getClassName(type, attrName, attrValue) {
+    const { selectedAttrs } = this.props;
+
+    if (type === 'swatch') {
+      const isSelected = selectedAttrs.attributes.find(
+        ({ name, value }) => name === attrName && value === attrValue,
+      );
+
+      if (isSelected) return 'attr-swatch-option attr-selected-swatch';
+      return 'attr-swatch-option';
+    }
+
+    const isSelected = selectedAttrs.attributes.find(
+      ({ name, value }) => name === attrName && value === attrValue,
+    );
+    if (isSelected) return 'attr-option attr-selected-option';
+    return 'attr-option';
+  }
+
+  async fetchProduct() {
+    const { selectedAttrs: { id } } = this.props;
+    const { data: { product } } = await client.query(
+      { query: GET_PRODUCT_BY_ID, variables: { id } },
+    );
+    this.setState({
+      product,
+    });
+  }
+
+  render() {
+    const { product, currency } = this.state;
+    const {
+      name, brand, attributes, prices, gallery,
+    } = product;
+    const { selectedAttrs } = this.props;
+    const { qty } = selectedAttrs;
+
+    return (
+      <div>
+        <div className="cart-product-container">
+          <div>
+            <h3 className="cart-product-brand">{ brand }</h3>
+            <h3 className="cart-product-name">{ name }</h3>
+
+            { prices && prices.map(({ currency: { symbol }, amount }) => (
+              symbol === currency && <p key={symbol} className="cart-product-price">{`${symbol}${amount * qty}`}</p>))}
+
+            { attributes && attributes.map(({ name: attrName, type, items }) => (
+              <>
+                <h4 className="cart-attr-name">{`${attrName}:`}</h4>
+                <div className="attr-option-container">
+                  {type === 'swatch'
+                    ? items.map(({ value }) => (
+                      <button
+                        aria-label="Color Option"
+                        key={value}
+                        type="button"
+                        className={this.getClassName(type, attrName, value)}
+                        style={{ backgroundColor: value }}
+                      />
+                    ))
+                    : items.map(({ value }) => (
+                      <button
+                        key={value}
+                        type="button"
+                        className={this.getClassName(type, attrName, value)}
+                      >
+                        {value}
+                      </button>
+                    ))}
+                </div>
+              </>
+            ))}
+
+          </div>
+          <div>
+            <div>
+              <button type="button">+</button>
+              <p>{ qty }</p>
+              <button type="button">-</button>
+            </div>
+            <img className="cart-product-img" src={gallery && gallery[0]} alt={name} />
+          </div>
+
+        </div>
+        <div className="cart-separating-line" />
+      </div>
+    );
+  }
+}
+
+export default CartProductCard;
